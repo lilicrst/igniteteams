0db3e88449107a9550d8289a130c0be2377de06b
lilicrst <lisandra.crisilva37@gmail.com>
2022-11-10 11:01:29 -0300

    feat: StatusBar, Loading component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import { ActivityIndicator } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import theme from './src/theme';
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
 
@@ -10,7 +11,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor= "transparent"
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
