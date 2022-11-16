3cf42dc748e3dc583204d8f71bd7d0cf2c9365e7
lilicrst <lisandra.crisilva37@gmail.com>
2022-11-16 12:38:33 -0300

    feat: EmptyList, Button component

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center">
-  <img src="https://github.com/lilicrst/igniteteams/blob/master/src/assets/logo.png" width="50" />
+  <img src="https://github.com/lilicrst/igniteteams/blob/master/src/assets/logo@2x.png" width="50" />
 </p>
 
-<h1 align="center">Ignite Teams</h1>
+<h2 align="center">Ignite Teams</h2>
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,33 +1,44 @@
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos', '6º período de computação']);
+  const [groups, setGroups] = useState<string[]>(['Grupo rocket']);
 
   return (
     <Container>
       <Header />
 
-      <HighLight 
+      <HighLight
         title='Turmas'
         subtitle='jogue com a sua turma'
       />
 
-      <FlatList 
+      <FlatList
         data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => (
-          <GroupCard 
-            title={item} 
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { marginTop: 100 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turma?'
           />
         )}
       />
 
+      <Button 
+        title='Criar nova turma'
+      />
     </Container>
-  );
+  )
 }
